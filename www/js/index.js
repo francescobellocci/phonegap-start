baca38c99cf6ce053e5e30cc5714aae7e23ddc53
Francesco Bellocci <bellocci.francesco@itismeucci.com>
2015-03-31 13:13:16 +0200

    schermata iniziale app incompleta

--- www/js/index.js
+++ www/js/index.js
@@ -46,16 +46,4 @@ var app = {
 
         console.log('Received Event: ' + id);
     }
-	
-	function bigImg(x) 
-	{
-		x.style.height = "64px";
-		x.style.width = "64px";
-	}
-
-	function normalImg(x) 
-	{
-		x.style.height = "32px";
-		x.style.width = "32px";
-	}
 };
